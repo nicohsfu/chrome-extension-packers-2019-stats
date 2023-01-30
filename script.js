e7af9df3a993df6a44534f6b80e020b32da5f97c
Nico Hernandez <marcnicohernandez@gmail.com>
2023-01-30 14:01:40 -0800

    assign key value pairs to html elements to display/render stats

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const fetchData = function() {
   const options = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': 'key goes here but I\m not pushing it to GitHub for privacy reasons',
+      'X-RapidAPI-Key': 'key goes here but I\'m not pushing it to GitHub for privacy reasons',
       'X-RapidAPI-Host': 'nfl-team-stats.p.rapidapi.com'
     }
   };
@@ -11,7 +11,12 @@ const fetchData = function() {
   fetch('https://nfl-team-stats.p.rapidapi.com/v1/nfl-stats/teams/receiving-stats/offense/2019', options)
     .then(response => response.json())
     .then(response => {
-      console.log("response : ", response);
+      // according to the API, packers is the 16th team on the list
+      const packers = response._embedded.teamReceivingStatsList[16];
+
+      document.getElementById("receives").innerHTML = packers.receives;
+      document.getElementById("touchdowns").innerHTML = packers.touchdowns;
+      document.getElementById("yards").innerHTML = packers.yards;
     })
     .catch(err => console.error(err));
 };
